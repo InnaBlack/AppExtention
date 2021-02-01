bf1a78155ae11bbbb022893b44364c21021312fd
InnaBlack <innablack85@gmail.com>
2021-02-01 18:43:09 +0100

    Initial Commit

---
+++ AppExtention/AppExtentionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppExtentionApp.swift
+//  AppExtention
+//
+//  Created by  inna on 01/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct AppExtentionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
